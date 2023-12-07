32b79a8a21af6b798f068e84f6db4304cf581403
fsossai <federico.sossai@gmail.com>
2023-12-07 02:25:07 -0600

    Added images

---
+++ plot_reduction.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+from scipy.stats import gmean
+import matplotlib
+import pathlib
+import pandas
+import numpy
+
+def plot(input_file):
+    df = pandas.read_csv(input_file)
+    r = df.pivot(columns="pipeline", index="input", values="r_out")
+    get_name = lambda x: pathlib.Path(x).name.split(".")[0]
+    r.index = list(map(get_name, r.index))
+
+    methods = ["LEX+VNS", "ML+VNS", "NN+VNS"]
+    lex = r.div(r["LEX"], axis=0)
+    print("Run counts reduction over LEX (geomean):")
+    for m in methods:
+        rcr = gmean(lex[m]) - 1
+        print("{:10}{:.2f} %".format(m, rcr*100))
+
+    #matplotlib.rcParams.update({'font.size': 16})
+
+    r.columns.name = ""
+    #plt.style.use("dark_background")
+    (lex[methods] - 1).plot.bar()
+    plt.xticks(rotation=30)
+    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+    plt.show()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=
+        "Plot the improvements over LEX given a benchmark result",
+        argument_default=argparse.SUPPRESS)
+
+    parser.add_argument("input", metavar="INPUT", type=str,
+        help="Benchmark results (CSV)")
+
+    args = parser.parse_args()
+    plot(args.input)
+
